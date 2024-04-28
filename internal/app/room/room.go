ad07ad9a9aefe895f6d8aae6232364d98749d2ba
pu4mane <pu4mane@gmail.com>
2024-04-28 18:22:44 +0300

    fix length message and exit

--- internal/app/room/room.go
+++ internal/app/room/room.go
@@ -10,6 +10,8 @@ import (
 	"github.com/pu4mane/goChat/internal/app/model"
 )
 
+const MAX_MESSAGE_LENGTH = 1000
+
 type ChatRoom struct {
 	clients  sync.Map
 	messages chan model.Message
@@ -21,18 +23,40 @@ func NewChatRoom() *ChatRoom {
 	}
 }
 
-func (cs *ChatRoom) HandleClient(id string, conn net.Conn) {
+func (cs *ChatRoom) AddClient(id string, conn net.Conn) {
 	cs.clients.Store(id, conn)
 	log.Println("User", id, "connected to the chat")
+}
+
+func (cs *ChatRoom) RemoveClient(id string) {
+	cs.clients.Delete(id)
+	log.Println("User", id, "left the chat")
+}
+
+func (cs *ChatRoom) HandleClient(id string, conn net.Conn) {
+	defer conn.Close()
+
+	cs.AddClient(id, conn)
 
 	input := bufio.NewScanner(conn)
 	for input.Scan() {
-		cs.messages <- model.Message{Text: id + ": " + input.Text(), ID: id}
+		message := input.Text()
+		//добавил ограничение символов
+		if len(message) > MAX_MESSAGE_LENGTH {
+			fmt.Fprintf(conn, "message is too long! (%d / %d maximum allowed characters)\n",
+				len(message),
+				MAX_MESSAGE_LENGTH)
+			continue
+		}
+		//добавил выход
+		if message == "/q" {
+			break
+		}
+
+		cs.messages <- model.Message{Text: id + ": " + message, ID: id}
 	}
 
-	cs.clients.Delete(id)
-	log.Println("User", id, "left the chat")
-	conn.Close()
+	cs.RemoveClient(id)
 }
 
 func (cs *ChatRoom) Broadcaster() {
